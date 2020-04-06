7738de004dcd469cba034348970bf0622df905c8
alim150102 <alim150102@gmail.com>
2020-04-06 16:39:39 +0700

    Pengenalan useState(), komponen Statefull & Stateless, Dan cara menambahkan Css pada aplikasi react

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
+import personCss from './Person/Person.css';
 
-const App = props => {
+class App extends Component {
 
-  const [personState, setPersonState] = useState(
-    {
+
+
+  state = {
+    persons: [
+      { nama: 'ali', umur: 18 },
+      { nama: 'fanny', umur: 16 }
+    ]
+  }
+
+  switchHandler = newName => {
+    this.setState({
+      persons: [
+        { nama: newName, umur: 16 },
+        { nama: 'shofie', umur: 14 }
+      ]
+    })
+  }
+  changeHandler = event => {
+    this.setState({
       persons: [
         { nama: 'ali', umur: 18 },
-        { nama: 'fanny', umur: 16 }
+        { nama: event.target.value, umur: 16 }
       ]
-    }
-  );
+    })
+  }
 
-  const [otherState, setOtherState] = useState("this is other value");
-  console.log(personState, otherState);
+  render() {
 
+    const style = {
+      width: '15 %',
+      height: '10 % px',
+      border: '1px solid #eee',
+      boxShadow: '0 3px 5px silver',
+      textAlign: 'center',
+      marginBottom: '20px',
+      padding: '16px',
+    };
 
-  const switchHandler = () => {
-    setPersonState({
-      persons: [
-        { nama: 'hirano', umur: 16 },
-        { nama: 'shofie', umur: 14 }
-      ],
-      otherState: personState.otherState
-    });
-  };
-
-  return (
-    <div className='App'>
-      <h1>Hai Buddy</h1>
-      <button onClick={switchHandler}>Switch Name</button>
-      <Person nama={personState.persons[0].nama} umur={personState.persons[0].umur} />
-      <Person nama={personState.persons[1].nama} umur={personState.persons[1].umur}> My Hobby is Swimming</Person>
-    </div>
-  );
-};
+    return (
+      <div className='App'>
+        <h1>Hai Buddy</h1>
+        <button className='button' onClick={this.switchHandler.bind(this, 'Maximilian')} style={style}>Switch Name</button>
+        <Person nama={this.state.persons[0].nama} umur={this.state.persons[0].umur}> My Hobby is Sleeping</Person>
+        <Person nama={this.state.persons[0].nama} umur={this.state.persons[1].umur} click={this.switchHandler.bind(this, 'Max')} changed={this.changeHandler}> My Hobby is Farming</Person>
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Person from './Person.css';
 
 const person = props => {
     return (
-        <div>
-            <p>Hai I'm  {props.nama} and i'm {props.umur} years old</p>
-            <p>{props.children}</p>
+        <div className='person'>
+            <p onClick={props.click}>Hai I'm  {props.nama} and i'm {props.umur} years old</p>
+            <p>{props.children}</p><br></br>
+            <input type="text" onChange={props.changed} className="input"></input>
         </div>
     )
 };
